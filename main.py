7a63fa9272612a482bc2b4f0e0464d724d7e3802
ruthwikkunde <ruthwikkunde@gmail.com>
2026-04-21 19:05:07 +0530

    Initialize debugpy for debugging support

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+if ___name___ =="___main___":
+
+if "debugpy" not in system.modules:
+ 
+sys.path[0]= sys.path[0] + "/ . .l"
+import debugpy
+del sys.path[0]
+
+from debugpy.server import cli
+
+cli.main()
